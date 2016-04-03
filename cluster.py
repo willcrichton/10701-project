f388b7a9a8b3f8984f6298204a4923d1010a0d59
Will Crichton <willcrichton@cmu.edu>
2016-04-03 13:44:55 -0400

    Added neural net

--- cluster.py
+++ cluster.py
@@ -5,8 +5,9 @@ from datetime import date, timedelta, datetime
 ACLED_KEYS = ['GWNO','EVENT_ID_CNTY','EVENT_ID_NO_CNTY','EVENT_DATE','YEAR','TIME_PRECISION','EVENT_TYPE','ACTOR1','ALLY_ACTOR_1','INTER1','ACTOR2','ALLY_ACTOR_2','INTER2','INTERACTION','COUNTRY','ADMIN1','ADMIN2','ADMIN3','LOCATION','LATITUDE','LONGITUDE','GEO_PRECISION','SOURCE','NOTES','FATALITIES']
 
 COUNTRY = 'egypt'
-WINDOW_SIZE = 30 # days
+WINDOW_SIZE = 14 # days
 NUM_CLUSTERS = 12
+CROSSVAL_K = 10
 
 def cluster_latlong():
     (conn, c) = get_db()
@@ -33,10 +34,12 @@ def cluster_latlong():
     # plot(clusters[:,0],clusters[:,1],'sg',markersize=8)
     # show()
 
+
 def get_db():
     conn = sqlite3.connect('{}.db'.format(COUNTRY))
     c = conn.cursor()
     return (conn, c)
+
 
 def csv_to_sqlite():
     import csv
@@ -64,59 +67,72 @@ def csv_to_sqlite():
     conn.commit()
     conn.close()
 
+
 def daterange(start_date, end_date):
+    """ Returns an iterator for all days between [start_date] and [end_date]. """
     for n in range(int ((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
-def kfold_cv(Classifier, xs, ys, k):
-    pk = len(xs) / k
-    avg_error = 0.0
-    for i in range(k):
-        cls = Classifier()
-        ki = pk * i
-        kj = pk * (i + 1)
-        xs_train = xs[:ki] + xs[kj:]
-        ys_train = ys[:ki] + ys[kj:]
-        xs_test = xs[ki:kj]
-        ys_test = ys[ki:kj]
-        cls.fit(xs_train, ys_train)
-        y_pred = cls.predict(xs_test)
-        (wrong,) = (ys_test != y_pred).sum(),
-        total = len(xs_test)
-        error = float(wrong) / total
-        avg_error += error
-    return avg_error / k
 
 def train_classifier():
     (conn, c) = get_db()
     clusters = np.load('clusters.npy')
     indices = np.load('cluster_mapping.npy')
 
-    cluster_error = 0.0
-    for cy in range(len(clusters)):
-        xs = []
-        ys = []
-        for day in daterange(date(2013, 1, 1), date(2015, 12, 31)):
-            c.execute('SELECT id, event_date FROM events WHERE event_date >= ? AND event_date <= ?',
-                      [day - timedelta(days=WINDOW_SIZE), day + timedelta(days=1)])
-            x = [0 for _ in range(len(clusters))]
-            y = 0
-            for (idx, event_date_s) in c.fetchall():
-                cluster = indices[idx]
-                event_date = datetime.strptime(event_date_s, '%Y-%m-%d 00:00:00')
-                if event_date.date() == day:
-                    if cluster == cy: y = 1
-                else:
-                    x[cluster] += 1 #WINDOW_SIZE - (event_date.date() - day).days
-            xs.append(x)
-            ys.append(y)
+    print 'Generating samples...'
+    xs = []
+    ys = []
+    for day in daterange(date(2013, 1, 1), date(2015, 12, 31)):
+        c.execute('SELECT id, event_date FROM events WHERE event_date >= ? '
+                  'AND event_date <= ?',
+                  [day - timedelta(days=WINDOW_SIZE), day + timedelta(days=1)])
+        x = [0 for _ in range(len(clusters))]
+        y = [0 for _ in range(len(clusters))]
+        for (idx, event_date_s) in c.fetchall():
+            cluster = indices[idx]
+            event_date = datetime.strptime(event_date_s, '%Y-%m-%d 00:00:00')
+            if event_date.date() == day:
+                y[cluster] = 1
+            else:
+                x[cluster] += 1 #WINDOW_SIZE - (event_date.date() - day).days
+        xs.append(x)
+        ys.append(y)
+    xs = np.array(xs)
+    ys = np.array(ys)
 
-        from sklearn.naive_bayes import GaussianNB
-        k = 10
-        kerr = kfold_cv(GaussianNB, xs, ys, k)
-        print 'k-CV for {}: {}'.format(cy, kerr)
-        cluster_error += kerr
-    print 'Average k-CV: {}'.format(cluster_error / len(clusters))
+    print 'Training...'
+    from sklearn import cross_validation as cv
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.base import clone
+    kf = cv.KFold(len(ys), CROSSVAL_K)
+
+    # cluster_error = 0.0
+    # gnb = GaussianNB()
+    # for cy in range(len(clusters)):
+    #     ys2 = ys[:, cy]
+    #     scores = cv.cross_val_score(gnb, xs, ys2, cv=kf)
+    #     print 'k-CV for {}: {} (+/- {:2})'.format(cy, scores.mean(), scores.std() * 2)
+    #     cluster_error += scores.mean()
+    # print 'Average k-CV: {}'.format(cluster_error / len(clusters))
+
+    from sknn import mlp
+    clf = mlp.Classifier(
+        layers=[mlp.Layer("Sigmoid", units=NUM_CLUSTERS),
+                mlp.Layer("Softmax")],
+        learning_rate=0.02,
+        n_iter=10)
+    clf.fit(xs, ys)
+    for i in range(1000):
+        print clf.predict(np.array([xs[i,:]]))
+    # for train, test in kf:
+    #     print 'Train xs', xs[train]
+    #     print 'Train ys', ys[train]
+    #     c = clone(clf)
+    #     c.fit(xs[train], ys[train])
+    #     print 'Predicted', c.predict(xs[test])
+    #     print 'Actual', ys[test]
+    #     #print (c.predict(xs[test]) != ys[test]).mean()
+
 
 def main():
     #cluster_latlong()
